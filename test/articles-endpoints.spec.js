52919a9102e60f12e15fa78f0da36cb586bf065d
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-06 23:10:07 -0800

    refactored post validation

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -104,34 +104,25 @@ describe(`Articles Endpoint`, function() {
                 )
         })
 
-        it(`responds with 4400 and an error message when the 'title' is missing`, () => {
-            return supertest(app)
-                .post('/articles')
-                .send({
-                    style: 'Listicle',
-                    content: 'Test new article content...'
-                })
-                .expect(400, { error: { message: `Missing 'title' in request body` }})
-        })
+        const requiredFields = ['title', 'style', 'content']
 
-        it(`responds with 400 and an error message when the 'content' is missing`, () => {
-            return supertest(app)
-                .post('/articles')
-                .send({
-                    title: 'Test new article',
-                    style: 'Listicle'
-                })
-                .expect(400, { error: { message: `Missing 'content' in request body`}})
-        })
+        requiredFields.forEach(field => {
+            const newArticle = {
+                title: 'Test new article',
+                style: 'Listicle',
+                content: 'Test new article content...'
+            }
 
-        it(`responds with 400 and an error message when the 'style' is missing`, () => {
-            return supertest(app)
-                .post('/articles')
-                .send({
-                    title: 'Test new article',
-                    content: 'Test new article content...'
-                })
-                .expect(400, { error: { message: `Missing 'style' in request body`}})
+            it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+                delete newArticle[field]
+
+                return supertest(app)
+                    .post('/articles')
+                    .send(newArticle)
+                    .expect(400, {
+                        error: { message: `Missing '${field}' in request body`}
+                    })
+            })
         })
     })
 })
