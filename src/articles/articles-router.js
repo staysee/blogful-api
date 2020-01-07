db8726d3d80bb095f8be9c4ca9f2390d92479e88
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-06 22:56:31 -0800

    refactored POST endpoints

---
+++ src/articles/articles-router.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const ArticlesService = require('./articles-service')
+
+const articlesRouter = express.Router()
+const jsonParser = express.json()
+
+articlesRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        ArticlesService.getAllArticles(knexInstance)
+            .then(articles => {
+                res.json(articles)
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { title, content, style } = req.body
+        const newArticle = { title, content, style }
+        const knexInstance = req.app.get('db')
+        ArticlesService.insertArticle(knexInstance, newArticle)
+            .then(article => {
+                res
+                    .status(201)
+                    .location(`/articles/${article.id}`)
+                    .json(article)
+            })
+            .catch(next)
+    })
+
+articlesRouter
+    .route('/:article_id')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        ArticlesService.getById(knexInstance, req.params.article_id)
+            .then(article => {
+                if (!article){
+                    return res.status(404).json({
+                        error: { message: `Article doesn't exist` }
+                    })
+                }
+                res.json(article)
+            })
+            .catch(next)
+    })
+
+module.exports = articlesRouter
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -3,7 +3,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const { makeArticlesArray } = require('./articles.fixtures')
 
-describe.only(`Articles Endpoint`, function() {
+describe(`Articles Endpoint`, function() {
     let db 
 
     before(`make knex instance`, () => {
@@ -75,7 +75,7 @@ describe.only(`Articles Endpoint`, function() {
         })
     })
 
-    describe.only(`POST /articles`, () => {
+    describe(`POST /articles`, () => {
         it(`creates an article, responding with 201 and the new article`, () => {
             this.retries(3)
             const newArticle = {
